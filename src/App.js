07732f5bc322c832e6fe07626a01f771d5a480a3
Srivastava57harsh <srivastava57harsh@gmail.com>
2022-02-22 23:54:07 +0530

    :sparkles: add background
    add background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
-
+import Hero from './components/Hero';
+import AboutUS from './components/AboutUs';
+import TopPlaylists from './components/TopPlaylists';
 function App() {
   return (
-    <div className="bg-black h-[100vh] w-[100vw]">
-     <h1>IndieHub</h1>
+    <div className="bg-[#10061E] h-[100vh] overflow-x-hidden scrollbar-thin scrollbar-thumb-scrollCol overflow-y-scroll">
+      <div className="">
+      <Hero />
+      <AboutUS/>
+      <TopPlaylists/>
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+export default function Hero(){
+    return(
+    <div>
+        <img src="/Ellipse 1.svg" className="w-[100vw] h-[100vh]"/>
+        
+    </div>
+    );
+
+}
