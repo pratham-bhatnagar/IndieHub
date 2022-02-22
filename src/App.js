6aa1f1882e037f035f3aa6011a5e61313b112006
Pratham Bhatnagar <84272138+pratham-bhatnagar@users.noreply.github.com>
2022-02-23 00:32:59 +0530

    :sparkles: tried prallax
    tried prallax

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="bg-black h-[100vh] w-[100vw]">
      <h1>IndieHub</h1>
+     <Navbar/>
+
     </div>
   );
 }
